20fcf254d9ff29043b5ad9f892539efaf03874d9
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2024-01-26 14:13:32 +0900

    6.2 Deps  영화 검색할때 숫자 카운트는 변하지 않았으면 할때
    렌더링을 방지할때 사용

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev +1);
-  console.log("i run all the time");
-  useEffect( () => { console.log("CALL THE API...") ;}, []);
+  const onChange = (event) => setKeyword(event.target.value);
+ 
+  useEffect( () => { 
+    console.log("i run only once") ;
+  }, []);
+  // 만약 검색을하면 오직 변화하는건 keyword만 되게 하는 컴포넌트
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("i run when 'keyword' changes.", keyword)
+    }
+    
+  }, [keyword]);
+  useEffect(() => {
+      console.log("i run when 'counter' changes.")
+  }, [counter])
+  useEffect(() => {
+    console.log("i run when 'keyword & counter' changes.")
+}, [keyword, counter])
   return (
     <div>
+      <input 
+      value = {keyword}
+      onChange={onChange}
+      type="text" placeholder="Search here.."/>
       <h1 >{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
