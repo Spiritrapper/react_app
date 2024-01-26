6586bbc7ef5ec02bcd958e88742e71d239ded311
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2024-01-26 14:52:51 +0900

    6.4 Cleanup
    useEffect 생성,소멸로써 사용

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import {useState, useEffect} from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev +1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // 한번만 실행되고 마는 코드
-  useEffect( () => { 
-    console.log("i run only once") ;
-  }, []);
-  // 만약 검색을하면 오직 변화하는건 keyword만 되게 하는 컴포넌트
+function Hello(){
+
+  /*function byFn(){
+    console.log("bye :(")
+  }
+  function hiFn() {
+    console.log("create :)")
+    return byFn;
+  }
+  // [] 컴포넌트를 생성했다가 없앨때 사용 가능
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>*/
+
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("i run when 'keyword' changes.", keyword)
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  },[]);
+  return <h1>Hello</h1>
+
+
+  /*useEffect(function(){
+    console.log("hi :)");;
+    return function(){
+      console.log("bye :(")
     }
-    
-  }, [keyword]);
-  useEffect(() => {
-      console.log("i run when 'counter' changes.")
-  }, [counter])
-  useEffect(() => {
-    console.log("i run when 'keyword & counter' changes.")
-}, [keyword, counter])
+  },[])
+  return <h1>Hello</h1>*/
+
+
+  /* useEffect(() => {
+    console("created :)")
+    return ()=> console.log("destroyed")
+  },[]) // [] 컴포넌트를 생성했다가 없앨때 사용 가능
+  return <h1>Hello</h1> */
+};
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-      value = {keyword}
-      onChange={onChange}
-      type="text" placeholder="Search here.."/>
-      <h1 >{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello/>:null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
