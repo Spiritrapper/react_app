0c364b6dec9930f559faef834996a007b2fa7070
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2024-01-27 16:21:25 +0900

    라우터 설치 명령 npm i react-router-dom@5.3.0

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import {useState, useEffect} from "react";
-import Movie from "./Movie";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const getMovies = async()=> {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )).json();
-      //const json = await response.json();
-      setMovies(json.data.movies);
-      setLoading(false);
-      };
-  useEffect (() => {
-    getMovies()
-  }, [])
-  console.log(movies)
-  return (
-    <div>
-      {loading ? <h1>Loading...</h1> : 
-      <div>
-        {movies.map((movie) => (
-        <Movie 
-        key={movie.id}
-        coverImg={movie.medium_cover_image} 
-        title={movie.title} 
-        summary={movie.summary} 
-        genres={movie.genres}
-        /> 
-        ))};
-      </div>
-    }
-       
-    </div>
-  )
+  return(<Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+        </Route>
+      <Route path="/">
+      <Home />
+      </Route>
+    </Switch>
+  </Router>
+  );
 
 }
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 function Movie({coverImg, title, summary, genres}) {
     return (
     <div >
     <img src={coverImg} alt={`${title} cover`} />
     <h2>{title}</h2>
+    <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
     <p>{summary}</p>
     <ui>
       {genres.map((g) => (
